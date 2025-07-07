1df64e9fe22ab56f7d31d8839c352ab7663c063a
assets72729 <hunxutia@gmail.com>
2025-07-07 18:44:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,17 +13,17 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "24890303"))
+API_HASH = getenv("API_HASH", "94cf78d1e6883ecb10f32e31fc23cfe0")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7289855833").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+LOG_GROUP = getenv("LOG_GROUP", "-1002670671084")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002705791364"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "6"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
-WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+WEBSITE_URL = getenv("WEBSITE_URL", "easysky.in")
+AD_API = getenv("AD_API", "3278575ec4cd0b5ecbb667144aedb749a6610ac3")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
 DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
