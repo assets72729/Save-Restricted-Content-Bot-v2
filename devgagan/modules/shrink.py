556a1d6774d4eb94bc3b9ca3e2f82828a622ab7b
assets72729 <hunxutia@gmail.com>
2025-07-21 19:00:37 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,9 +73,9 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://graph.org/file/94bf9089d43b817bb76d2-c0ccb0de00b10adf9e.jpg"
+        image_url = "https://graph.org/file/94bf9089d43b817bb76d2-c0ccb0de00b10adf9e.png"
         join_button = InlineKeyboardButton("Updates Channel", url="https://t.me/smartkitbots")
-        premium = InlineKeyboardButton("How To Use Me", url="https://t.me/smartkitbots/5")   
+        premium = InlineKeyboardButton("How To Use Me", url="https://t.me/smartkitbots/8")   
         keyboard = InlineKeyboardMarkup([
             [join_button],   
             [premium]    
