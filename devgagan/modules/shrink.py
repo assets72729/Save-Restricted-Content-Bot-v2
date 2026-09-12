33acf33ee23acfdd1f028f6790b3052ed32d202b
assets72729 <hunxutia@gmail.com>
2026-09-12 09:07:48 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,9 +73,9 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://graph.org/file/94bf9089d43b817bb76d2-c0ccb0de00b10adf9e.png"
-        join_button = InlineKeyboardButton("Updates Channel", url="https://t.me/smartkitbots")
-        premium = InlineKeyboardButton("How To Use Me", url="https://t.me/smartkitbots/28")   
+        image_url = "https://ibb.co/KxyZYWWy"
+        join_button = InlineKeyboardButton("Updates Channel", url="https://t.me/tubi_bots")
+        premium = InlineKeyboardButton("Get Premium", url="t.me/TubiSupportBot")   
         keyboard = InlineKeyboardMarkup([
             [join_button],   
             [premium]    
@@ -85,8 +85,8 @@ async def token_handler(client, message):
             msg.photo.file_id,
             caption=(
                 "Hi 👋 Welcome, Wanna intro...?\n\n"
-                "👉 What I Can Do:\n✨ Save posts from channels and groups where forwarding is off.\n✨ Easily fetch messages from public channels by sending their post links.\n✨ For private channels, use /login to access content securely.\n✨ Need assistance? Just type /help and I'll guide you!\n"
-                "\n👉 Premium Features:\n🔹 Use /token to get 3 hours of free premium access.\n🔹 Want unlimited access? Use /Plan to unlock premium features.\n🔹 Premium users enjoy faster processing, unlimited saves, and priority support.\n\n📌 Getting Started:\n✅ Send a post link from a public channel to save it instantly.\n✅ If the channel is private, use /login before sending the link.\n✅ For additional commands, check /help anytime! \n\nHappy saving!🚀"
+                "👉 What I Can Do:\n✨ Save posts from channels and groups where forwarding is off.\n✨ Easily fetch messages from public channels by sending their post links.\n✨ For private channels, use /login to access content securely.\n✨ Need assistance? Just type /settings and I'll guide you!\n"
+                "\n👉 Premium Features:\n🔹 Use /token to get 3 hours of free premium access.\n🔹 Want unlimited access? Use /Plan to unlock premium features.\n🔹 Premium users enjoy faster processing, unlimited saves, and priority support.\n\n📌 Getting Started:\n✅ Send a post link from a public channel to save it instantly.\n✅ For additional commands, check /help anytime! \n\nHappy saving!🚀"
             ),
             reply_markup=keyboard
         )
