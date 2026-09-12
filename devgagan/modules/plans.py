85492ce569b15be19346a7059de666e994950a41
assets72729 <hunxutia@gmail.com>
2026-09-12 08:59:25 +0530

    Update plans.py

--- devgagan/modules/plans.py
+++ devgagan/modules/plans.py
@@ -159,7 +159,7 @@ async def transfer_premium(client, message):
                 f"👤 **From:** {sender_user.mention}\n"
                 f"👤 **To:** {new_user.mention}\n"
                 f"⏳ **Expiry Date:** {expiry_str_in_ist}\n\n"
-                f"__Powered by Joseph__ 🚀"
+                f"__Powered by Tubi Bots__ 🚀"
             )
             
             # Notification to the new user
